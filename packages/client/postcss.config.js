94ad77ab8afe3490900a3c813d4bbaebc34d752a
李宇 <lijingyu68@gmail.com>
2020-09-22 14:40:57 +0800

    modify npm script serve to start, and add postcss-preset-env.

--- packages/client/postcss.config.js
+++ packages/client/postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: [
-    require('autoprefixer'),
-    require('postcss-nested'),
     require('postcss-import'),
+    require('postcss-nested'),
+    require('postcss-preset-env'),
+    require('autoprefixer'),
   ],
 }
